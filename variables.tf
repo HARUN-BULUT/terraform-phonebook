fec738a900cd9c425e83ad43cf7e63a66094db86
harun <harunbulutus@gmail.com>
2024-04-01 04:04:41 +0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "HARUN-BULUT"
+}
+
+variable "git-token" {
+    default = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "devopsharun.click"
+}
